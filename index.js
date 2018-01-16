5ea5b1b6a2618f340b672d328781237c915f27a3
ff05 <frank.filius@gmail.com>
2018-01-16 15:04:03 +0100

    Create index.js, config/database.js and models/profile.js to start setting up the routes

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+
+app.get('/', (req, res) => {
+  
+})
+
+app.post('/', (req, res) => {
+  
+})
+
+app.patch('/', (req, res) => {
+  
+})
+
+app.listen()
