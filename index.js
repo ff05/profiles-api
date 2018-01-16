cde95acd264c348752b24d1b20fc623d4041a862
ff05 <frank.filius@gmail.com>
2018-01-16 16:27:16 +0100

    Edit endpoints

--- index.js
+++ index.js
@@ -1,21 +1,54 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 const cors = require('cors')
 
+app
+.use(cors())
+.use(bodyParser.urlencoded({ extended: true }))
+.use(bodyParser.json())
+
 app.get('/', (req, res) => {
-  
+  Profile.find()
+    .then((profiles) => res.json(profiles))
+    .catch((error) => next(error))
 })
 
 app.post('/', (req, res) => {
-  
+  let newProfile = req.body
+
+  Profile.create(newProfile)
+  .then(profile => res.json(profile))
+  .catch(error => next(error))
 })
 
 app.patch('/:id', (req, res) => {
-  
+  const id = req.params.id
+  const patchForProfile = req.body
+
+  Profile.findById(id)
+    .then((profile) => {
+      if (!profile) { return next() }
+
+      Profile.findByIdAndUpdate(id, { $set: patchForProfile }, { new: true })
+        .then((updatedProfile) => res.json(updatedProfile))
+        .catch((error) => next(error))
+    })
+    .catch((error) => next(error))
 })
 
 app.delete('/:id', (req, res) => {
-  
+  const id = req.params.id
+
+  Profile.findByIdAndRemove(id)
+    .then(() => {
+      res.status = 200
+      res.json({
+        message: 'Removed',
+        _id: id
+      })
+    })
+    .catch((error) => next(error))
 })
 
 app.listen()
